ff062875fe1c636877edea20af0ffc5d6b9dfab3
Karan Aggarwal <Karanaggarwal2008@gmail.com>
2020-12-13 17:04:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,7 +34,7 @@ function setup() {
   trex = createSprite(50,180,20,50);
   trex.addAnimation("running", trex_running);
   trex.addAnimation("collided",trex_collided);
-  trex.scale = 0.5;
+  trex.scale = 0.1;
   
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
